f6778ca30d437bc4b9c414b5d46eb03a8a276d13
Richard Weeks <rich@controlcode.co.uk>
2018-03-21 09:27:23 +0000

    Add template

--- cc/corp/views.py
+++ cc/corp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello World!")
+    context={}
+    template = loader.get_template('corp/index.html')
+    return HttpResponse(template.render(context, request))
